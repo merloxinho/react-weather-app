cff53f2afb8152bef2f81393a042a1e5ce5a5a61
merlox <150959690+merloxinho@users.noreply.github.com>
2024-02-24 21:19:20 +0100

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './index.css';
 import Weatherapp from './components/Weatherapp';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
   return (
     <>
       <Weatherapp />
+      <Analytics />
     </>
   )
 }
